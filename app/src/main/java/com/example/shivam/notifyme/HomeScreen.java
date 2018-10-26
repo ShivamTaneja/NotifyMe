37eb35a68f3fb40e64b7b4c389333db1ada493fa
ShivamTaneja <shivamtaneja2211997@gmail.com>
2018-10-26 15:12:04 +0530

    2 - added on click listener to floating action button

--- app/src/main/java/com/example/shivam/notifyme/HomeScreen.java
+++ app/src/main/java/com/example/shivam/notifyme/HomeScreen.java
@@ -1,7 +1,10 @@
 package com.example.shivam.notifyme;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AddYourTask.class);
+                startActivity(intent);
+            }
+        });
     }
 }
