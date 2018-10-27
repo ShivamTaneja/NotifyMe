68868c596c81dc75b3460cb5998efb5c6f32a047
ShivamTaneja <shivamtaneja2211997@gmail.com>
2018-10-27 16:53:29 +0530

    4 - updated add your task java file

--- app/src/main/java/com/example/shivam/notifyme/AddYourTask.java
+++ app/src/main/java/com/example/shivam/notifyme/AddYourTask.java
@@ -1,13 +1,86 @@
 package com.example.shivam.notifyme;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class AddYourTask extends AppCompatActivity {
+import java.util.Calendar;
+
+public class AddYourTask extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    EditText editTask; // task name
+    Spinner spin; // spinner for type of task and their values are stored in taskNames;
+    CalendarView calendarViewFrom; // to choose dates from
+    CalendarView calendarViewTo; // to choose dates upto
+    TimePicker timePickerNotifyAt;// time at which notification will appear
+    CheckBox makeItAHabit;
+    Button buttonSave;
+
+    String[] taskNames = {"Health & fitness", "Study", "Work", "Meeting", "Shopping", "Entertainment", "Relax", "Travel", "Family Time", "Others"};
+    String taskNameInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_your_task);
+
+        //task
+        editTask = findViewById(R.id.editTask);
+        taskNameInput = editTask.getText().toString();
+
+        //spinner
+        spin = findViewById(R.id.simple_spinner);
+        spin.setOnItemSelectedListener(AddYourTask.this);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, taskNames);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(arrayAdapter);
+
+        //calendar
+        calendarViewFrom = findViewById(R.id.simpleCalendarViewFrom);
+        calendarViewTo = findViewById(R.id.simpleCalendarViewTo);
+
+
+        buttonSave = findViewById(R.id.buttonSave);
+        buttonSave.setOnClickListener ( new View . OnClickListener () {
+                                            @Override
+                                            public void onClick(View view) {
+
+                                     }
+                                        }
+            );
+
+        makeItAHabit = findViewById(R.id.checkboxMakeItAHabit);
+
+
+        //time picker
+        timePickerNotifyAt = findViewById(R.id.timepickerNotifyAt);
+        timePickerNotifyAt.setIs24HourView(false); // used to display AM/PM mode
+        // perform set on time changed listener event
+        timePickerNotifyAt.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+
+            }
+        });
+}
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
