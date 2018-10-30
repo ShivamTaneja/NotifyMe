e9317573d0844438fade5670d2dbef17dd36217a
ShivamTaneja <shivamtaneja2211997@gmail.com>
2018-10-31 02:45:12 +0530

    9 - modifications in nav view

--- app/src/main/java/com/example/shivam/notifyme/Activity/HomeScreen.java
+++ app/src/main/java/com/example/shivam/notifyme/Activity/HomeScreen.java
@@ -1,7 +1,6 @@
 package com.example.shivam.notifyme.Activity;
 
 import android.content.Intent;
-import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,7 +12,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
-import android.support.v7.widget.Toolbar;
+
 import com.example.shivam.notifyme.R;
 import com.stephentuso.welcome.WelcomeHelper;
 
@@ -36,7 +35,7 @@ public class HomeScreen extends AppCompatActivity {
     private static final String TAG_ABOUT_US = "about us";
     public static String CURRENT_TAG = TAG_HOME;
 
-    // flag to load home fragment when user presses back key
+    // flag to load home activity when user presses back key
     private boolean shouldLoadHomeFragOnBackPress = true;
 
     @Override
@@ -49,7 +48,6 @@ public class HomeScreen extends AppCompatActivity {
 
         // load toolbar titles from string resources
         activityTitles = getResources().getStringArray(R.array.nav_item_activity_titles);
-
 
         welcomeScreen = new WelcomeHelper(this, WelcomeScreenActivity.class);
         welcomeScreen.show(savedInstanceState);
@@ -88,7 +86,7 @@ public class HomeScreen extends AppCompatActivity {
     }
 
     /***
-     * Returns respected fragment that user
+     * Returns respected activity that user
      * selected from navigation menu
      */
     private void loadHome() {
@@ -101,37 +99,10 @@ public class HomeScreen extends AppCompatActivity {
             drawer.closeDrawers();
             return;
         }
+
         //Closing drawer on item click
         drawer.closeDrawers();
-        // refresh toolbar menu
-        invalidateOptionsMenu();
-    }
 
-    private void getHome() {
-        Intent intent;
-        switch (navItemIndex) {
-            case 0:
-                // home
-                drawer.closeDrawers();
-            case 1:
-               intent = new Intent(this, Performance.class);
-               startActivity(intent);
-                drawer.closeDrawers();
-            case 2:
-                intent = new Intent(this,Notifications.class);
-                startActivity(intent);
-                drawer.closeDrawers();
-            case 3:
-                intent = new Intent(this,Settings.class);
-                startActivity(intent);
-                drawer.closeDrawers();
-            case 4:
-                intent = new Intent(this,AboutUs.class);
-                startActivity(intent);
-                drawer.closeDrawers();
-            default:
-                drawer.closeDrawers();
-        }
     }
 
     private void selectNavMenu() {
@@ -146,31 +117,47 @@ public class HomeScreen extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
 
+                Intent intent;
                 //Check to see which item was being clicked and perform appropriate action
                 switch (menuItem.getItemId()) {
                     //Replacing the main content with ContentFragment Which is our Inbox View;
                     case R.id.nav_home:
                         navItemIndex = 0;
                         CURRENT_TAG = TAG_HOME;
+                        // home
+                        drawer.closeDrawers();
                         break;
                     case R.id.nav_performance:
                         navItemIndex = 1;
                         CURRENT_TAG = TAG_PERFORMANCE;
+                        intent = new Intent(getApplicationContext(), Performance.class);
+                        startActivity(intent);
+                        drawer.closeDrawers();
                         break;
                     case R.id.nav_notifications:
                         navItemIndex = 2;
                         CURRENT_TAG = TAG_NOTIFICATIONS;
+                        intent = new Intent(getApplicationContext(),Notifications.class);
+                        startActivity(intent);
+                        drawer.closeDrawers();
                         break;
                     case R.id.nav_settings:
                         navItemIndex = 3;
                         CURRENT_TAG = TAG_SETTINGS;
+                        intent = new Intent(getApplicationContext(),Settings.class);
+                        startActivity(intent);
+                        drawer.closeDrawers();
                         break;
                     case R.id.nav_about_us:
                         navItemIndex = 4;
                         CURRENT_TAG = TAG_ABOUT_US;
+                        intent = new Intent(getApplicationContext(),AboutUs.class);
+                        startActivity(intent);
+                        drawer.closeDrawers();
                         break;
                     default:
                         navItemIndex = 0;
+                        drawer.closeDrawers();
                 }
 
                 //Checking if the item is in checked state or not, if not make it in checked state
@@ -193,7 +180,7 @@ public class HomeScreen extends AppCompatActivity {
             public void onDrawerClosed(View drawerView) {
                 // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
                 super.onDrawerClosed(drawerView);
-            }
+                }
 
             @Override
             public void onDrawerOpened(View drawerView) {
@@ -230,37 +217,5 @@ public class HomeScreen extends AppCompatActivity {
         }
 
         super.onBackPressed();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-
-        if (navItemIndex == 2) {
-            getMenuInflater().inflate(R.menu.notifications, menu);
-        }
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        // user is in notifications fragment
-        // and selected 'Mark all as Read'
-        if (id == R.id.action_mark_all_read) {
-            Toast.makeText(getApplicationContext(), "All notifications marked as read!", Toast.LENGTH_LONG).show();
-        }
-
-        // user is in notifications fragment
-        // and selected 'Clear All'
-        if (id == R.id.action_clear_notifications) {
-            Toast.makeText(getApplicationContext(), "Clear all notifications!", Toast.LENGTH_LONG).show();
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/shivam/notifyme/Activity/Notifications.java
+++ app/src/main/java/com/example/shivam/notifyme/Activity/Notifications.java
@@ -2,6 +2,9 @@ package com.example.shivam.notifyme.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.shivam.notifyme.R;
 
@@ -12,4 +15,35 @@ public class Notifications extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.notifications, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        // user is in notifications fragment
+        // and selected 'Mark all as Read'
+        if (id == R.id.action_mark_all_read) {
+            Toast.makeText(getApplicationContext(), "All notifications marked as read!", Toast.LENGTH_LONG).show();
+        }
+
+        // user is in notifications fragment
+        // and selected 'Clear All'
+        if (id == R.id.action_clear_notifications) {
+            Toast.makeText(getApplicationContext(), "Clear all notifications!", Toast.LENGTH_LONG).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
+
